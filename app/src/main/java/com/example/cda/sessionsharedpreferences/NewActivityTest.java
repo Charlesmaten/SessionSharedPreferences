7edb7239e083d5cc1f978429f8a6cadc5128551b
unknown <cda@Charles-Work.It-afdelingen.local>
2015-09-14 15:14:36 +0200

    first commit

---
+++ app/src/main/java/com/example/cda/sessionsharedpreferences/NewActivityTest.java
@@ -0,0 +1,76 @@
+package com.example.cda.sessionsharedpreferences;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+public class NewActivityTest extends AppCompatActivity {
+
+    SessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        // Session class instance
+        session = new SessionManager(getApplicationContext());
+
+        TextView lblName = (TextView) findViewById(R.id.textViewName);
+        TextView lblEmail = (TextView) findViewById(R.id.textViewEmail);
+
+
+        Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
+
+
+        /**
+         * Call this function whenever you want to check user login
+         * This will redirect user to LoginActivity is he is not
+         * logged in
+         * */
+        session.checkLogin();
+
+        // get user data from session
+        HashMap<String, String> user = session.getUserDetails();
+
+        // name
+        String name = user.get(SessionManager.KEY_NAME);
+
+        // email
+        String email = user.get(SessionManager.KEY_EMAIL);
+
+        // displaying user data
+        lblName.setText(Html.fromHtml("Name: <b>" + name + "</b>"));
+        lblEmail.setText(Html.fromHtml("Email: <b>" + email + "</b>"));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_activity_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
